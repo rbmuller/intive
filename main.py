40591ea620698364af6321955c14c55856cde78a
Robson Muller <rbmuller91@gmail.com>
2024-03-23 16:41:48 -0300

    Adding parameter validation

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import json
 from typing import List, Optional, Dict, Any
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
-from pyspark.sql import SparkSession, functions as F
+from pyspark.sql import SparkSession, DataFrame, functions as F
 from pyspark.sql.window import Window
 from helpers.data_cleaner import GetCleanData, create_spark_session
 
 app = FastAPI()
 
 # Initialize Spark session and ingest the data
-spark = create_spark_session()
-df = GetCleanData(spark, "data/sales_data.csv").cache()  # Cache the DataFrame for reuse
+try:
+    spark = create_spark_session()
+    df = GetCleanData(
+        spark, "data/sales_data.csv"
+    ).cache()  # Cache the DataFrame for reuse
+except Exception as e:
+    app.logger.error(f"Failed to load sales data: {e}")
+    raise HTTPException(
+        status_code=500, detail="Failed to initialize Spark session or ingest data"
+    )
 
 
 # Response models
@@ -37,13 +45,21 @@ class AvgSalesPriceResponse(BaseModel):
 
 # Helper function to apply filters
 def apply_filters(
-    df, store_name: Optional[str], product_category: Optional[str]
-) -> F.DataFrame:
+    df: DataFrame, store_name: Optional[str], product_category: Optional[str]
+) -> DataFrame:
     if store_name:
         df = df.filter(F.col("store_name") == store_name)
     if product_category:
         df = df.filter(F.col("product_category") == product_category)
     return df
+
+
+# Helper function to check if DataFrame is empty
+def check_empty_data(df: DataFrame):
+    if df.rdd.isEmpty():
+        raise HTTPException(
+            status_code=404, detail="No data found with specified filters."
+        )
 
 
 @app.get("/")
@@ -63,11 +79,15 @@ async def root() -> Dict[str, Any]:
 
 @app.get("/total_revenue_per_store", response_model=List[StoreSales])
 async def total_sales_per_store(store_name: Optional[str] = Query(None)):
-    filtered_df = apply_filters(df, store_name, None)
-    result_df = filtered_df.groupBy("store_name").agg(
-        F.sum("sales_amount").alias("total_revenue")
-    )
-    return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    try:
+        filtered_df = apply_filters(df, store_name, None)
+        check_empty_data(filtered_df)
+        result_df = filtered_df.groupBy("store_name").agg(
+            F.sum("sales_amount").alias("total_revenue")
+        )
+        return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/total_products_per_store", response_model=List[TotalProductCount])
@@ -75,22 +95,30 @@ async def total_products_per_store(
     store_name: Optional[str] = Query(None),
     product_category: Optional[str] = Query(None),
 ):
-    filtered_df = apply_filters(df, store_name, product_category)
-    result_df = (
-        filtered_df.groupBy("store_name")
-        .count()
-        .withColumnRenamed("count", "qty_products_sold")
-    )
-    return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    try:
+        filtered_df = apply_filters(df, store_name, product_category)
+        check_empty_data(filtered_df)
+        result_df = (
+            filtered_df.groupBy("store_name")
+            .count()
+            .withColumnRenamed("count", "qty_products_sold")
+        )
+        return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/avg_sales_price_per_category", response_model=List[AvgSalesPriceResponse])
 async def avg_sales_price_per_category(product_category: Optional[str] = Query(None)):
-    filtered_df = apply_filters(df, None, product_category)
-    result_df = filtered_df.groupBy("product_category").agg(
-        F.avg("sales_amount").alias("average_sales_price")
-    )
-    return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    try:
+        filtered_df = apply_filters(df, None, product_category)
+        check_empty_data(filtered_df)
+        result_df = filtered_df.groupBy("product_category").agg(
+            F.avg("sales_amount").alias("average_sales_price")
+        )
+        return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/top_selling_products", response_model=List[TopSellingProduct])
@@ -98,16 +126,20 @@ async def top_selling_products(
     store_name: Optional[str] = Query(None),
     product_category: Optional[str] = Query(None),
 ):
-    filtered_df = apply_filters(df, store_name, product_category)
-    window_spec = Window.partitionBy("store_name").orderBy(F.desc("total_sales"))
-    result_df = (
-        filtered_df.groupBy("store_name", "product_category")
-        .agg(F.sum("sales_amount").alias("total_sales"))
-        .withColumn("rank", F.rank().over(window_spec))
-        .filter(F.col("rank") == 1)
-        .drop("rank")
-    )
-    return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    try:
+        filtered_df = apply_filters(df, store_name, product_category)
+        check_empty_data(filtered_df)
+        window_spec = Window.partitionBy("store_name").orderBy(F.desc("total_sales"))
+        result_df = (
+            filtered_df.groupBy("store_name", "product_category")
+            .agg(F.sum("sales_amount").alias("total_sales"))
+            .withColumn("rank", F.rank().over(window_spec))
+            .filter(F.col("rank") == 1)
+            .drop("rank")
+        )
+        return result_df.toJSON().map(lambda x: json.loads(x)).collect()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
